6a7ab4aad136fc1d5afd894df74ff71f85d91ee6
Jonas Rinke <delphi1024@gmail.com>
2024-12-14 17:18:55 +0100

    Defined tile drawing for SkyliteTarget

--- crates/skylite-core/src/lib.rs
+++ crates/skylite-core/src/lib.rs
@@ -25,6 +25,26 @@ pub trait SkyliteTarget {
     /// clockwise. Rotation is applied after flipping.
     fn draw_sub(&mut self, data: &[u8], x: i16, y: i16, src_x: i16, src_y: i16, src_w: u16, src_h: u16, flip_h: bool, flip_v: bool, rotate: bool);
 
+    /// Draws a region from a texture atlas to the screen as a tile.
+    ///
+    /// The specifics of how tiles are drawn depends on the target, but
+    /// they should generally have a fixed size and be layed out in a regular grid.
+    /// Tiles can be drawn on different layers, where each layer may have separate
+    /// tile sizes, offsets and other configurations, depending on the capabilities
+    /// of the target.
+    ///
+    /// `tile_x_idx` and `tile_y_idx` give the x and y index into the tile grid at which
+    /// the tile should be drawn. `src_x` and `src_y` denote the top-left coordinate
+    /// in the texture atlas in pixels. `flip_h`, `flip_y` and `rotate` work the same
+    /// as in `draw_sub`.
+    fn draw_tile(&mut self, data: &[u8], layer: u8, tile_x_idx: i16, tile_y_idx: i16, src_x: i16, src_y: i16, flip_h: bool, flip_v: bool, rotate: bool);
+
+    /// Changes some configurable aspect of a layer. The parameters
+    /// available and the corresponding values for `config` are
+    /// specific to each target, but could include things like
+    /// the tile dimensions or the current scroll offset.
+    fn layer_cfg(&mut self, layer: u8, config: u32, data: &[u8]);
+
     /// Returns the screen size of the target as a (width, height) tuple.
     /// This must always return the same value during the lifetime of the instance.
     fn get_screen_size(&self) -> (u16, u16);
--- crates/skylite-mock/src/lib.rs
+++ crates/skylite-mock/src/lib.rs
@@ -18,6 +18,22 @@ pub enum Call {
         flip_v: bool,
         rotate: bool
     },
+    DrawTile {
+        data: u64,
+        layer: u8,
+        tile_x_idx: i16,
+        tile_y_idx: i16,
+        src_x: i16,
+        src_y: i16,
+        flip_h: bool,
+        flip_v: bool,
+        rotate: bool
+    },
+    LayerCfg {
+        layer: u8,
+        config: u32,
+        data: Vec<u8>
+    },
     SaveState {
         location: String,
         data: Vec<u8>
@@ -44,9 +60,16 @@ fn apply_transform(pos: (i16, i16), w: u16, h: u16, flip_h: bool, flip_v: bool, 
     }
 }
 
+pub struct MockLayerCfg {
+    pub tile_size: u8,
+    pub x_off: u8,
+    pub y_off: u8
+}
+
 pub struct MockTarget {
     pub call_history: Vec<Call>,
     pub screen_buffer: [u8; 128 * 128],
+    pub layer_cfg_data: [MockLayerCfg; 4],
     pub state: Vec<u8>
 }
 
@@ -56,7 +79,25 @@ impl MockTarget {
         MockTarget {
             call_history: Vec::new(),
             screen_buffer: [0; 128 * 128],
+            layer_cfg_data: [
+                MockLayerCfg { tile_size: 16, x_off: 0, y_off: 0 },
+                MockLayerCfg { tile_size: 16, x_off: 0, y_off: 0 },
+                MockLayerCfg { tile_size: 16, x_off: 0, y_off: 0 },
+                MockLayerCfg { tile_size: 16, x_off: 0, y_off: 0 },
+            ],
             state: Vec::new()
+        }
+    }
+
+    fn draw_sub_impl(&mut self, data: &[u8], x: i16, y: i16, src_x: i16, src_y: i16, src_w: u16, src_h: u16, flip_h: bool, flip_v: bool, rotate: bool) {
+        let data_width = data[data.len() - 1] as i16;
+        for offset_y in 0..src_h as i16 {
+            for offset_x in 0..src_w as i16 {
+                let src_index = (src_y + offset_y) * data_width + src_x + offset_x;
+                let screen_offset = apply_transform((offset_x, offset_y), src_w, src_h, flip_h, flip_v, rotate);
+                let screen_index = (y + screen_offset.1) * 128 + x + screen_offset.0;
+                self.screen_buffer[screen_index as usize] = data[src_index as usize];
+            }
         }
     }
 }
@@ -68,14 +109,28 @@ impl SkyliteTarget for MockTarget {
         hasher.write(data);
         self.call_history.push(Call::DrawSub { data: hasher.finish(), x, y, src_x, src_y, src_w, src_h, flip_h, flip_v, rotate });
 
-        let data_width = data[data.len() - 1] as i16;
-        for offset_y in 0..src_h as i16 {
-            for offset_x in 0..src_w as i16 {
-                let src_index = (src_y + offset_y) * data_width + src_x + offset_x;
-                let screen_offset = apply_transform((offset_x, offset_y), src_w, src_h, flip_h, flip_v, rotate);
-                let screen_index = (y + screen_offset.1) * 128 + x + screen_offset.0;
-                self.screen_buffer[screen_index as usize] = data[src_index as usize];
-            }
+        self.draw_sub_impl(data, x, y, src_x, src_y, src_w, src_h, flip_h, flip_v, rotate);
+    }
+
+    fn draw_tile(&mut self, data: &[u8], layer: u8, tile_x_idx: i16, tile_y_idx: i16, src_x: i16, src_y: i16, flip_h: bool, flip_v: bool, rotate: bool) {
+        let mut hasher = DefaultHasher::new();
+        hasher.write(data);
+        self.call_history.push(Call::DrawTile { data: hasher.finish(), layer, tile_x_idx, tile_y_idx, src_x, src_y, flip_h, flip_v, rotate });
+
+        assert!(layer <= 3);
+        let cfg = &self.layer_cfg_data[layer as usize];
+        self.draw_sub_impl(data, tile_x_idx * (cfg.tile_size as i16) - (cfg.x_off as i16), tile_y_idx * (cfg.tile_size as i16) - (cfg.y_off as i16),
+                            src_x, src_y, cfg.tile_size as u16, cfg.tile_size as u16, flip_h, flip_v, rotate);
+    }
+
+    fn layer_cfg(&mut self, layer: u8, config: u32, data: &[u8]) {
+        self.call_history.push(Call::LayerCfg { layer, config, data: data.to_owned() });
+        assert!(layer <= 3);
+        match config {
+            0 => self.layer_cfg_data[layer as usize].tile_size = data[0],
+            1 => self.layer_cfg_data[layer as usize].x_off = data[0],
+            2 => self.layer_cfg_data[layer as usize].y_off = data[0],
+            _ => panic!("Unknown config {}", config)
         }
     }
 
