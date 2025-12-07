5e7524462bbe9239d9fed0a87414ca36dca2c74f
Jonas Rinke <delphi1024@gmail.com>
2025-12-07 17:03:32 +0100

    Moved sequence compilation into asset server

--- crates/skylite-assets/asset-server/sequences.rkt
+++ crates/skylite-assets/asset-server/sequences.rkt
@@ -6,16 +6,22 @@
 (provide refine-sequence)
 
 
-(define (get-nested-property-type start path retrieve-node)
-  (for/fold ([acc (retrieve-node start)])
-            ([segment path])
-    ; Will fail if the previous segment did not resolve to a node.
-    (unless (node? acc) (raise-asset-error "Unable to resolve property ~a" segment))
-    (define property (assq (string->symbol segment) (node-properties acc)))
+(define (process-property-access start path retrieve-node)
+  (let lp ([path path]
+           [node-name start]
+           [push-offset-ops '()])
+    (define segment (car path))
+    (define current-node (retrieve-node node-name))
+    (define property (assq (string->symbol segment) (node-properties current-node)))
     (unless property (raise-asset-error "Unable to resolve property ~a" segment))
-    (match (cadr property)
-      [(cons 'node name) (retrieve-node name)]
-      [type type])))
+    (define prop-name (car property))
+    (define prop-type (cadr property))
+    (define inst `(push-offset ,(symbol->string node-name) ,(symbol->string prop-name)))
+    (if (null? (cdr path))
+        (values (reverse (cons inst push-offset-ops)) prop-type)
+        (if (eq? (car prop-type) 'node)
+            (lp (cdr path) (cdr prop-type) (cons inst push-offset-ops))
+            (raise-asset-error "Intermediate property ~a is not a node" prop-name)))))
 
 
 ; When a 'forwards' local label is used in a jump/branch instruction,
@@ -30,15 +36,26 @@
 
 (define (get-jump-target inst)
   (match inst
-    [(list 'branch _ target) target]
+    [(list 'branch-if-true target) target]
+    [(list 'branch-if-false target) target]
+    [(list 'branch-uint _ _ target) target]
+    [(list 'branch-sint _ _ target) target]
+    [(list 'branch-f32 _ _ target) target]
+    [(list 'branch-f64 _ _ target) target]
     [(list 'jump target) target]
     [(list 'branch-custom _ target) target]
+    ; call instructions are not handled in this step
     [_ #f]))
 
 
 (define (set-jump-target inst target)
   (match inst
-    [(list 'branch cond _) (list 'branch cond target)]
+    [(list 'branch-if-true _) (list 'branch-if-true target)]
+    [(list 'branch-if-false _) (list 'branch-if-false target)]
+    [(list 'branch-uint op value _) (list 'branch-uint op value target)]
+    [(list 'branch-sint op value _) (list 'branch-sint op value target)]
+    [(list 'branch-f32 op value _) (list 'branch-f32 op value target)]
+    [(list 'branch-f64 op value _) (list 'branch-f64 op value target)]
     [(list 'jump _) (list 'jump target)]
     [(list 'branch-custom fname _) (list 'branch-custom fname target)]
     [_ inst]))
@@ -179,6 +196,30 @@
   (apply append prepared-scripts))
 
 
+(define (resolve-labels script)
+  (define label->pos
+    (make-immutable-hash
+     (let lp ([script script]
+              [pos 0]
+              [alist '()])
+       (cond
+         [(null? script) alist]
+         [(string? (car script)) (lp (cdr script) pos (cons (cons (car script) pos) alist))]
+         [else (lp (cdr script) (+ pos 1) alist)]))))
+  (for/list ([item script] #:when (list? item))
+    (match item
+      [(list 'branch-if-true target) `(branch-if-true ,(hash-ref label->pos target))]
+      [(list 'branch-if-false target) `(branch-if-false ,(hash-ref label->pos target))]
+      [(list 'branch-uint op value target) `(branch-uint ,op ,value ,(hash-ref label->pos target))]
+      [(list 'branch-sint op value target) `(branch-sint ,op ,value ,(hash-ref label->pos target))]
+      [(list 'branch-f32 op value target) `(branch-f32 ,op ,value ,(hash-ref label->pos target))]
+      [(list 'branch-f64 op value target) `(branch-f64 ,op ,value ,(hash-ref label->pos target))]
+      [(list 'jump target) `(jump ,(hash-ref label->pos target))]
+      [(list 'call target) `(call ,(hash-ref label->pos target))]
+      [(list 'branch-custom fname target) `(branch-custom ,fname ,(hash-ref label->pos target))]
+      [inst inst])))
+
+
 (define (numeric-type? type)
   (memq type '(u8 u16 u32 u64 i8 i16 i32 i64 f32 f64)))
 
@@ -190,79 +231,103 @@
     (let ([e (assq 'node asset-data)])
       (or (cdr e) (raise-user-error "Missing require key 'node"))))
 
-  ; Validate correct syntax and add type information to immediate values.
-  (define (refine-branch-condition condition)
+  (define (compile-set prop value)
+    (unless (symbol? prop) (raise-asset-error "Expected symbol for 'set' instruction, got ~v" prop))
+    (define path (string-split (symbol->string prop) "."))
+    (define-values (push-offset-ops prop-type) (process-property-access target-node path retrieve-node))
+    (define refined-value (refine-value prop-type value asset-exists? retrieve-node))
+    (append push-offset-ops
+            ; set-string is a separate instruction, because String in Rust
+            ; is heap-allocated, so it must be handled differently than other base types.
+            (if (eq? prop-type 'string)
+                (list `(set-string ,refined-value))
+                (list `(set (,prop-type . ,refined-value))))))
+
+  (define (compile-modify prop value)
+    (unless (symbol? prop) (raise-asset-error "Expected symbol for 'modify' instruction, got ~v" prop))
+    (define path (string-split (symbol->string prop) "."))
+    (define-values (push-offset-ops prop-type) (process-property-access target-node path retrieve-node))
+    (unless (numeric-type? prop-type)
+      (raise-asset-error "'modify' can only be used for fields with numeric types."))
+    (define refined-value (refine-value prop-type value asset-exists? retrieve-node))
+    (append push-offset-ops
+            (case prop-type
+              [(f32) (list `(modify-f32 ,refined-value))]
+              [(f64) (list `(modify-f64 ,refined-value))]
+              [else (list `(modify (,prop-type . ,refined-value)))])))
+
+  (define (compile-branch condition target)
+    (define (comparison->byte comp)
+      (case comp [(=) 2] [(!=) 3] [(<) 4] [(>) 5] [(<=) 6] [(>=) 7]))
+
+    (unless (symbol? target) (raise-asset-error "Target for 'branch' instruction must be a symbol, got ~v" target))
     (match condition
       [(list op prop value)
        (unless (memq op '(= != < > <= >=))
          (raise-asset-error "Invalid comparison operation for branch condition: ~a" op))
        (define path (string-split (symbol->string prop) "."))
-       (define property-type (get-nested-property-type target-node path retrieve-node))
-       (when (and (memq op '(< > <= >=)) (not (numeric-type? property-type)))
+       (define-values (push-offset-ops prop-type) (process-property-access target-node path retrieve-node))
+       (unless (numeric-type? prop-type)
          (raise-asset-error "Comparison is only allowed for numeric types"))
-       `(,op ,path (,property-type . ,(refine-value property-type value asset-exists? property-type)))]
+       (define refined-value (refine-value prop-type value asset-exists? retrieve-node))
+       (append push-offset-ops
+               (case prop-type
+                 [(u8 u16 u32 u64) (list `(branch-uint ,(comparison->byte op) (,prop-type . ,refined-value) ,target))]
+                 [(i8 i16 i32 i64) (list `(branch-sint ,(comparison->byte op) (,prop-type . ,refined-value) ,target))]
+                 [(f32) (list `(branch-f32 ,(comparison->byte op) ,refined-value ,target))]
+                 [(f64) (list `(branch-f64 ,(comparison->byte op) ,refined-value ,target))]))]
 
       [(list '! prop)
        (define path (string-split (symbol->string prop) "."))
-       (define property-type (get-nested-property-type target-node path retrieve-node))
-       (unless (eq? property-type 'bool) (raise-asset-error "Branch if false is only allowed for bool properties"))
-       `(! ,path)]
+       (define-values (push-offset-ops prop-type) (process-property-access target-node path retrieve-node))
+       (unless (eq? prop-type 'bool) (raise-asset-error "Branch if false is only allowed for bool properties"))
+       (append push-offset-ops
+               (list `(branch-if-false ,target)))]
 
       [(or prop (list prop))
        #:when (symbol? prop)
        (define path (string-split (symbol->string prop) "."))
-       (define property-type (get-nested-property-type target-node path retrieve-node))
-       (unless (eq? property-type 'bool) (raise-asset-error "Branch if true is only allowed for bool properties"))
-       path]
+       (define-values (push-offset-ops prop-type) (process-property-access target-node path retrieve-node))
+       (unless (eq? prop-type 'bool) (raise-asset-error "Branch if true is only allowed for bool properties"))
+       (append push-offset-ops
+               (list `(branch-if-true ,target)))]
 
       [_ (raise-asset-error "Invalid branch condition ~v" condition)]))
 
   ; Validate correct syntax and add type information to immediate values.
-  (define (refine-instruction inst)
+  (define (compile-instruction inst)
     (match inst
-      [(list 'set prop value)
-       (unless (symbol? prop) (raise-asset-error "Expected symbol for 'set' instruction, got ~v" prop))
-       (define path (string-split (symbol->string prop) "."))
-       (define property-type (get-nested-property-type target-node path retrieve-node))
-       `(set ,path (,property-type . ,(refine-value property-type value asset-exists? property-type)))]
+      [(list 'set prop value) (compile-set prop value)]
 
-      [(list 'modify prop value)
-       (unless (symbol? prop) (raise-asset-error "Expected symbol for 'set' instruction, got ~v" prop))
-       (define path (string-split (symbol->string prop) "."))
-       (define property-type (get-nested-property-type target-node path retrieve-node))
-       (unless (numeric-type? property-type)
-         (raise-asset-error "'modify' can only be used for fields with numeric types."))
-       `(modify ,path (,property-type . ,(refine-value property-type value asset-exists? property-type)))]
+      [(list 'modify prop value) (compile-modify prop value)]
 
-      [(list 'branch condition target)
-       (unless (symbol? target) (raise-asset-error "Target for 'branch' instruction must be a symbol, got ~v" target))
-       `(branch ,(refine-branch-condition condition) ,target)]
+      [(list 'branch condition target) (compile-branch condition target)]
 
       [(list 'jump target)
        (unless (symbol? target) (raise-asset-error "Target for 'jump' instruction must be a symbol, got ~v" target))
-       inst]
+       (list inst)]
 
       [(list 'call sub)
        (unless (symbol? sub) (raise-asset-error "Target for 'call' instruction must be a symbol, got ~v" sub))
-       inst]
+       (list inst)]
 
-      [(list 'return) inst]
+      [(list 'return) (list inst)]
 
       [(list 'wait frames)
        (when (or (not (exact? frames)) (< frames 0))
          (raise-asset-error "'wait' instruction expects a positive integer, got ~v" frames))
-       inst]
+       (list inst)]
 
       [(list 'run-custom fname)
        (unless (symbol? fname)
          (raise-asset-error "'run-custom' instruction expects a symbol as function name, got ~v" fname))
-       inst]
+       (list inst)]
 
       [(list 'branch-custom fname target)
        (unless (symbol? fname)
          (raise-asset-error "'run-custom' instruction expects a symbol as function name, got ~v" fname))
        (unless (symbol? target) (raise-asset-error "Target for 'branch-custom' instruction must be a symbol, got ~v" target))
-       inst]
+       (list inst)]
 
       [_ (raise-asset-error "Unknown instruction ~v" inst)]))
 
@@ -273,32 +338,34 @@
   (when (memq '$main sub-names)
     (raise-asset-error "'$main is not allowed as a subroutine name"))
 
-  (define (refine-script script name)
+  (define (compile-script script name)
     (define out
-      (for/list ([item script])
-        (cond
-          [(list? item) (refine-instruction item)]
-          [(symbol? item) item]
-          [else (raise-asset-error "Unknown syntax in script: ~v" item)])))
+      (apply append
+             (for/list ([item script])
+               (cond
+                 [(list? item) (compile-instruction item)]
+                 [(symbol? item) (list item)]
+                 [else (raise-asset-error "Unknown syntax in script: ~v" item)]))))
     (validate-labels out)
     (validate-calls out sub-names)
     (rename-labels out name))
 
-  (define refined-subs
+  (define subroutines
     (for/list ([pair sub-list])
       (unless (pair? pair)
         (raise-asset-error "'subs key must be an alist, got ~v" sub-list))
       (unless (symbol? (car pair)) (raise-asset-error "Subroutine name must be a symbol, got ~v" (car pair)))
-      (cons (car pair) (refine-script (cdr pair) (car pair)))))
-
+      pair))
   (define main-script (assq 'script asset-data))
   (unless main-script
     (raise-asset-error "Missing required key 'script"))
-  (define main-refined (refine-script (cdr main-script) '$main))
 
-  (define full-script (merge-scripts (cons (cons '$main main-refined) refined-subs)))
+  (define scripts (cons (cons '$main main-script) subroutines))
+  (define compiled-scripts (for/list ([pair scripts]) (cons (car pair) (compile-script (cdr pair) (car pair)))))
+  (define merged-script (merge-scripts compiled-scripts))
+  (define resolved-script (resolve-labels merged-script))
 
-  (sequence target-node full-script))
+  (sequence target-node resolved-script))
 
 
 (module+ test
@@ -306,7 +373,7 @@
 
   (define (retrieve-node name)
     (case name
-      [(node1) (node '() '([prop1 u8] [prop2 bool] [prop3 string] [prop4 (node . node2)]))]
+      [(node1) (node '() '([prop1 u8] [prop2 bool] [prop3 string] [prop4 (node . node2)] [prop5 f32] [prop6 f64] [prop7 i8]))]
       [(node2) (node '() '([prop1 u8]))]))
 
   (define (asset-exists? name)
@@ -318,32 +385,48 @@
       [subs . ([sub1 . ((return))])]
       [script
        . (- (set prop1 5)
+            (set prop3 "test")
             (modify prop1 5)
+            (modify prop5 1.0)
+            (modify prop6 -1.0)
             (branch prop2 -)
             (branch (! prop2) -)
-            (branch (= prop3 "test") -)
-            (branch (!= prop3 "xyz") -)
-            (branch (< prop1 10) -)
+            (branch (= prop1 5) -)
+            (branch (< prop7 6) -)
+            (branch (> prop5 5.0) -)
+            (branch (>= prop6 -5.0) -)
             (jump -)
             (call sub1)
             (run-custom test-fn)
             (branch-custom branch-fn -))]))
   (define refined1
-    '("sub-$main"
-      "$main-b---0"
-      (set ("prop1") (u8 . 5))
-      (modify ("prop1") (u8 . 5))
-      (branch ("prop2") "$main-b---0")
-      (branch (! ("prop2")) "$main-b---0")
-      (branch (= ("prop3") (string . "test")) "$main-b---0")
-      (branch (!= ("prop3") (string . "xyz")) "$main-b---0")
-      (branch (< ("prop1") (u8 . 10)) "$main-b---0")
-      (jump "$main-b---0")
-      (call "sub-sub1")
+    '((push-offset "node1" "prop1") ; 0
+      (set (u8 . 5))
+      (push-offset "node1" "prop3")
+      (set-string "test")
+      (push-offset "node1" "prop1")
+      (modify (u8 . 5))             ; 5
+      (push-offset "node1" "prop5")
+      (modify-f32 1.0)
+      (push-offset "node1" "prop6")
+      (modify-f64 -1.0)
+      (push-offset "node1" "prop2") ; 10
+      (branch-if-true 0)
+      (push-offset "node1" "prop2")
+      (branch-if-false 0)
+      (push-offset "node1" "prop1")
+      (branch-uint 2 (u8 . 5) 0)    ; 15
+      (push-offset "node1" "prop7")
+      (branch-sint 4 (i8 . 6) 0)
+      (push-offset "node1" "prop5")
+      (branch-f32 5 5.0 0)
+      (push-offset "node1" "prop6") ; 20
+      (branch-f64 7 -5.0 0)
+      (jump 0)
+      (call 27)
       (run-custom test-fn)
-      (branch-custom branch-fn "$main-b---0")
+      (branch-custom branch-fn 0)   ; 25
       (return)
-      "sub-sub1"
       (return)))
 
   (test-log! (equal? (sequence-script (refine-sequence seq1 asset-exists? retrieve-node)) refined1))
@@ -355,9 +438,11 @@
        . ((set prop1 5)
           (set prop4.prop1 10))]))
   (define refined2
-    '("sub-$main"
-      (set ("prop1") (u8 . 5))
-      (set ("prop4" "prop1") (u8 . 10))
+    '((push-offset "node1" "prop1")
+      (set (u8 . 5))
+      (push-offset "node1" "prop4")
+      (push-offset "node2" "prop1")
+      (set (u8 . 10))
       (return)))
 
   (test-log! (equal? (sequence-script (refine-sequence seq2 asset-exists? retrieve-node)) refined2))
@@ -373,16 +458,11 @@
             + (jump +)
             + (jump label))]))
   (define refined3
-    '("sub-$main"
-      "$main-b---0"
-      (jump "$main-b---0")
-      "$main-b----2" "$main-l-label"
+    '((jump 0)
       (wait 0)
-      (jump "$main-b----2")
-      (jump "$main-f-+-3")
-      "$main-f-+-3"
-      (jump "$main-f-+-1")
-      "$main-f-+-1"
-      (jump "$main-l-label")))
+      (jump 1)
+      (jump 4)
+      (jump 5)
+      (jump 1)))
 
   (test-log! (equal? (sequence-script (refine-sequence seq3 asset-exists? retrieve-node)) refined3)))
